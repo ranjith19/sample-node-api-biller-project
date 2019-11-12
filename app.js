1ffe9b5128496a86d3b0f3aedb4d7090cbaeb39b
Ranjith Ramachandra <ranjith19@gmail.com>
2019-11-12 08:23:36 +0530

    beautify

--- app.js
+++ app.js
@@ -6,70 +6,67 @@ var jwt = require('json-web-token');
 const app = express();
 var port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.json({
-    "message": 'Hello user!'
+  "message": 'Hello user!'
 }));
 
-app.post('/bills/fetch', function (request, response) {
-    let data = request.body;
-    console.log(data);
-    let headers = requee
-    response.json({
-      "status": 200,
-      "success": true,
-      "data": {
-        "customer": {
-          "name": "Ashok Kumar"
-        },
-        "billDetails": {
-          "billFetchStatus": "AVAILABLE",
-          "bills": [
-            {
-              "billerBillID": "12123131322",
-              "generatedOn": "2019-08-01T08:28:12Z",
-              "recurrence": "ONE_TIME",
-              "amountExactness": "EXACT",
-              "customerAccount": {
-                "id": "8208021440"
-              },
-              "aggregates": {
-                "total": {
-                  "displayName": "Total Outstanding",
-                  "amount": {
-                    "value": 99000
-                  }
-                }
+app.post('/bills/fetch', function(request, response) {
+  requestLogger(request);
+  response.json({
+    "status": 200,
+    "success": true,
+    "data": {
+      "customer": {
+        "name": "Ashok Kumar"
+      },
+      "billDetails": {
+        "billFetchStatus": "AVAILABLE",
+        "bills": [{
+          "billerBillID": "12123131322",
+          "generatedOn": "2019-08-01T08:28:12Z",
+          "recurrence": "ONE_TIME",
+          "amountExactness": "EXACT",
+          "customerAccount": {
+            "id": "8208021440"
+          },
+          "aggregates": {
+            "total": {
+              "displayName": "Total Outstanding",
+              "amount": {
+                "value": 99000
               }
             }
-          ]
-        }
+          }
+        }]
       }
-    });
+    }
+  });
 });
 
-app.post('/bills/fetchReceipt', function (request, response) {
-    let data = request.body;
-    console.log(data);
-    response.json({
-  "status": 200,
-  "success": true,
-  "data": {
-    "billerBillID": "12123131322",
-    "platformBillID": "SETU121341312121",
-    "platformTransactionRefID": "TXN12121219",
-    "receipt": {
-      "id": getRandomString(15),
-      "date": "2019-08-02T07:12:10Z"
+app.post('/bills/fetchReceipt', function(request, response) {
+  requestLogger(request);
+  response.json({
+    "status": 200,
+    "success": true,
+    "data": {
+      "billerBillID": "12123131322",
+      "platformBillID": "SETU121341312121",
+      "platformTransactionRefID": "TXN12121219",
+      "receipt": {
+        "id": getRandomString(15),
+        "date": "2019-08-02T07:12:10Z"
+      }
     }
-  }
   });
 });
 
 
-function requestLogger(request){
+function requestLogger(request) {
   let rid = getRandomString(10);
   let body = request.body;
   let headers = request.headers;
@@ -79,18 +76,18 @@ function requestLogger(request){
 
 
 function getRandomString(length) {
-   var result           = '';
-   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-   var charactersLength = characters.length;
-   for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-   }
-   return result;
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
-function makeJwtSecret(numParts){
+function makeJwtSecret(numParts) {
   let parts = [];
-  for(let i=0; i<numParts; i++){
+  for (let i = 0; i < numParts; i++) {
     parts.push(getRandomString(6))
   }
   return parts.join("-");
@@ -98,6 +95,6 @@ function makeJwtSecret(numParts){
 
 
 
-var server = app.listen(port, function () {
-    console.log('Server running at http://127.0.0.1:' + port + '/');
+var server = app.listen(port, function() {
+  console.log('Server running at http://127.0.0.1:' + port + '/');
 });
